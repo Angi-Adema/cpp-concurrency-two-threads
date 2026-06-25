0851852ae45bfa85ca30132746582e69568fc989
Angi-Adema <dr.adema@gmail.com>
2026-06-25 13:07:06 -0600

    added logic for a counter and incrementCounter() and decrementCounter() methods.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <stdexcept>
+#include <mutex>
+#include <thread>
 
 using namespace std;
 
+// Create a mutex object to lock access to shared resources
+mutex mtx;
+
 int main() {
-	cout << "Hello World!" << endl;
+
 }
